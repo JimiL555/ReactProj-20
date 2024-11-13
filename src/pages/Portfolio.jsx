970f17e3498eebc5555a094585988c3cff757bac
Jimi <liapis.jsl@gmail.com>
2024-11-13 01:33:43 -0500

    Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,12 +5,18 @@ import Project from '../components/Project';
 function Portfolio() {
   const projects = [
     {
-      title: 'Project 1',
-      image: 'image-url-1.jpg',
-      deployedLink: 'https://live-site-1.com',
-      repoLink: 'https://github.com/JimiL555/project-1',
+      title: 'Group Project 2',
+      image: 'path-to-image-1.jpg', // Image or screenshot for Project 1
+      deployedLink: 'https://seismic-tracker-pro.onrender.com/search',
+      repoLink: 'https://github.com/JimiL555/GRP2-Seismic-Tracker-Pro.git',
     },
-    // Add more projects here
+    {
+      title: 'A day in Phish History App',
+      image: '/Users/jsliapis/Documents/Bootcamp/My-Projects/ReactProj-20/src/images/Phish_Project.jpeg', // Image or screenshot for Project 2
+      deployedLink: 'https://jimil555.github.io/GRPJ-1-Phish-/',
+      repoLink: 'https://github.com/JimiL555/GRPJ-1-Phish-.git',
+    },
+    // Add more projects similarly...
   ];
 
   return (
