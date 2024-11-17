9b265b984604558dcdeefa9c8ff7c39d0081265c
Jimi <liapis.jsl@gmail.com>
2024-11-17 01:32:26 -0500

    deployed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ function Footer() {
   return (
     <footer>
       <a href="https://github.com/JimiL555" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="https://linkedin.com/in/jimiL" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/james-liapis-94248a31a" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
       <a href="https://stackoverflow.com/users/12345678/jimiL" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
     </footer>
   );
